14843e93a75966f63d96ede0c4fa540dc97c8e58
tracyhuynh <thh220000@utdallas.edu>
2023-08-18 20:07:31 -0500

    updated frontend

---
+++ frontend/luffy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './app/about/page';
+import Header from './components/header';
+
+function App() {
+  return (
+    <div className="App">
+      <About />
+    </div>);
+}
+
+export default App;
